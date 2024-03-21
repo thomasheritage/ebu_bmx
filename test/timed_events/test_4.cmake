984eb59d02107afe00a0baff54dc2f3d04025a25
Philip de Nier <philipn@rd.bbc.co.uk>
2024-03-21 16:52:41 +0000

    timed_events: Add timed events tests

---
+++ test/timed_events/test_4.cmake
@@ -0,0 +1,30 @@
+# Test creating an MXF file containing video, timed events and an image resource.
+
+set(test_num 4)
+include("${TEST_SOURCE_DIR}/test_common.cmake")
+
+set(create_command ${RAW2BMX}
+    --regtest
+    -t op1a
+    -f 25
+    -y 10:00:00:00
+    -o ${output_file}
+    --timed-events "${TEST_SOURCE_DIR}/manifest_2.xml"
+    --avci100_1080i video_1_${test_num}
+)
+
+run_test_a(
+    "${TEST_MODE}"
+    "${BMX_TEST_WITH_VALGRIND}"
+    "${create_test_video_1}"
+    ""
+    ""
+    "${create_command}"
+    ""
+    ""
+    "${read_command}"
+    "${output_file}"
+    "test_4.md5"
+    "${output_info_file};info_4.xml.bin"
+    "${output_essence_file_prefix}_d0.xml;events_example.xml.bin;${output_essence_file_prefix}_d0_12.raw;image.png"
+)
